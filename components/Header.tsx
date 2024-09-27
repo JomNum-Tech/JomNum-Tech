260dc72599f56114c697160321028663fd442d08
Vathanak <vathanaksol1605@gmail.com>
2024-09-27 12:19:22 +0700

    add animation heading in homepage

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import React from 'react'
 import { ShapeSeven, Shapes } from './Icon';
 import Image from 'next/image';
 import Link from 'next/link';
+import TypingHeading from './TypingHeading';
 
 export const Header = () => {
 	return (
@@ -11,23 +14,21 @@ export const Header = () => {
 			<div className="container px-4 mx-auto">
 				<div className="grid grid-cols-12 gap-6 items-center">
 					<div className="col-span-12 lg:col-span-7 xl:col-span-6 text-center lg:text-start mb-12 lg:mb-0">
-						<h2 className="text-3xl font-bold leading-tight tracking-wide lg:text-7xl mb-6">
-							Say Goodbye To Manual
-						</h2>
-						<div className="max-w-xl">
-							<p className="text-[17px] leading-relaxed opacity-80 my-12">
+						<TypingHeading texts={['Say Goodbye To Manual', 'Say Hello To Automation!']} />
+						<div className="max-w-xl w-full ">
+							<p className="text-[17px] leading-relaxed opacity-80 my-12 w-full text-center">
 								Automation CI/CD Pipeline, streamline your workflow process
 							</p>
 						</div>
-						<main className="flex justify-between gap-12 w-full px-8">                                  
-                            <Link href="/deployment" className="w-[50%] text-center py-3 px-8 font-medium text-white bg-blue-600 hover:bg-opacity-70 rounded-full transition-all ease-in-out">
-                                <button>Getting Started</button>
-                            </Link>         
-                            <Link href="/document" className="w-[50%] text-center py-3 px-8 font-medium text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white border hover:bg-opacity-90 rounded-full transition-all ease-in-out">
-                                <button>Documentation</button>
-                            </Link>
-                        </main>
-                        
+						<main className="flex justify-between gap-12 w-full px-8">
+							<Link href="/deployment" className="w-[50%] text-center py-3 px-8 font-medium text-white bg-blue-600 hover:bg-opacity-70 rounded-full transition-all ease-in-out">
+								<button>Getting Started</button>
+							</Link>
+							<Link href="/document" className="w-[50%] text-center py-3 px-8 font-medium text-blue-500 border-blue-500 hover:bg-blue-500 hover:text-white border hover:bg-opacity-90 rounded-full transition-all ease-in-out">
+								<button>Documentation</button>
+							</Link>
+						</main>
+
 					</div>
 					<div className="col-span-12 lg:col-span-5 relative text-center">
 						<ShapeSeven />
@@ -35,8 +36,8 @@ export const Header = () => {
 						<div className="relative">
 							<Image
 								src="/assets/springOps.jpg"
-                                width={600}
-                                height={600}
+								width={600}
+								height={600}
 								alt=""
 								className="max-w-full h-auto rounded-full mx-auto"
 							/>
