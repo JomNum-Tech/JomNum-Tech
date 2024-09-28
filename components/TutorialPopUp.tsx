bf44a6c2be4a9f85ef1073f5e9922800d9bd910f
Vathanak <vathanaksol1605@gmail.com>
2024-09-28 11:17:44 +0700

    update tutorial popup modal

--- components/TutorialPopUp.tsx
+++ components/TutorialPopUp.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
-import { X, ChevronLeft, ChevronRight } from 'lucide-react'
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import Image from 'next/image';
 
 const tutorialSteps = [
@@ -12,19 +12,19 @@ const tutorialSteps = [
     image: "/assets/homepage.svg"
   },
   {
-    title: "Our Services",
-    content: "Explore our range of automation and deployment services.",
+    title: "Deployment Pipeline",
+    content: "Create automating workflows with GitHub and Jenkins.",
     image: "/assets/homepage.svg"
   },
   {
-    title: "Client and Partner",
-    content: "See how we've helped businesses like yours succeed.",
-    image: "/assets/homepage.svg"
+    title: "Learning Resource",
+    content: "Provide interactive learning and a vast array of resources.",
+    image: "/assets/learningpage.svg"
   },
   {
-    title: "Testimonials",
-    content: "Read what our satisfied clients have to say about us.",
-    image: "/assets/homepage.svg"
+    title: "Documentation",
+    content: "A ton of resources for software developer to deep dive with Spring and DevOps",
+    image: "/assets/documentpage.svg"
   },
   {
     title: "Get in Touch",
@@ -63,6 +63,24 @@ const TutorialPopUp: React.FC = () => {
     }
   }
 
+  useEffect(() => {
+    const handleKeyDown = (event: { key: string; }) => {
+      if (event.key === 'ArrowRight') {
+        nextStep();
+      } else if (event.key === 'ArrowLeft') {
+        prevStep();
+      } else if (event.key === 'Escape') {
+        closeTutorial();
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [currentStep, nextStep, prevStep]);
+
   return (
     <>
       <button
@@ -78,37 +96,56 @@ const TutorialPopUp: React.FC = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed inset-0 bg-black bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-90 flex items-center justify-center p-4 z-50"
+            className="fixed inset-0 bg-black bg-opacity-50 dark:bg-gray-900 dark:bg-opacity-90 flex items-center justify-center p-4 z-50 backdrop-blur-sm"
           >
             <motion.div
               initial={{ scale: 0.9, opacity: 0 }}
               animate={{ scale: 1, opacity: 1 }}
               exit={{ scale: 0.9, opacity: 0 }}
-              className="bg-white dark:bg-gray-800 rounded-lg shadow-xl p-8 max-w-md w-full relative"
+              className="bg-white dark:bg-gray-800 rounded-lg shadow-xl px-8 pt-4 pb-6 max-w-md w-full relative transition-transform duration-300 ease-in-out"
             >
-              <button
+              {/* <button
                 onClick={closeTutorial}
                 className="absolute top-2 right-2 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300"
                 aria-label="Close tutorial"
               >
                 <X className="w-6 h-6" />
+              </button> */}
+              <button onClick={closeTutorial} className="text-gray-500 flex justify-end w-full">
+                Skip Tutorial
               </button>
-              <div className="mb-4">
-                <Image
-                  src={tutorialSteps[currentStep].image}
-                  width={1200}
-                  height={200}
-                  alt={tutorialSteps[currentStep].title}
-                  className="w-full h-48 object-cover rounded-lg"
+              <div className="mb-4 pt-2">
+                <motion.div
+                  key={currentStep}
+                  initial={{ x: '100%', opacity: 0 }}
+                  animate={{ x: '0%', opacity: 1 }}
+                  exit={{ x: '-100%', opacity: 0 }}
+                  transition={{ duration: 0.5 }}
+                >
+                  <Image
+                    src={tutorialSteps[currentStep].image}
+                    width={1200}
+                    height={200}
+                    alt={tutorialSteps[currentStep].title}
+                    className="w-full h-48 object-cover rounded-lg shadow-md"
+                  />
+                </motion.div>
+              </div>
+              <div className="pb-16">
+                <h2 className="text-2xl font-bold mb-2 text-green-500">{tutorialSteps[currentStep].title}</h2>
+                <p className="mb-4 dark:text-gray-400">{tutorialSteps[currentStep].content}</p>
+              </div>
+              <div className="relative w-full h-2 bg-gray-200 dark:bg-gray-700 rounded">
+                <div
+                  className="absolute h-full bg-green-500 dark:bg-green-400 rounded"
+                  style={{ width: `${((currentStep + 1) / tutorialSteps.length) * 100}%` }}
                 />
               </div>
-              <h2 className="text-2xl font-bold mb-2 dark:text-white">{tutorialSteps[currentStep].title}</h2>
-              <p className="mb-4 dark:text-gray-400">{tutorialSteps[currentStep].content}</p>
-              <div className="flex justify-between items-center">
+              <div className="flex justify-between items-center pt-6">
                 <button
                   onClick={prevStep}
                   disabled={currentStep === 0}
-                  className="flex items-center justify-center w-10 h-10 rounded-full bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-400 disabled:opacity-50"
+                  className={`flex items-center justify-center w-10 h-10 rounded-full ${currentStep === 0 ? 'bg-gray-300' : 'bg-gray-200'} dark:bg-gray-700 text-gray-${currentStep === 0 ? '400' : '600'} disabled:opacity-50`}
                   aria-label="Previous step"
                 >
                   <ChevronLeft className="w-6 h-6" />
@@ -118,7 +155,7 @@ const TutorialPopUp: React.FC = () => {
                 </span>
                 <button
                   onClick={nextStep}
-                  className="flex items-center justify-center w-10 h-10 rounded-full bg-primary text-primary-foreground dark:bg-primary-dark dark:text-primary-foreground-dark"
+                  className={`flex items-center justify-center w-10 h-10 rounded-full bg-primary text-primary-foreground ${currentStep === tutorialSteps.length - 1 ? 'bg-green-500' : ''} dark:bg-primary-dark dark:text-primary-foreground-dark`}
                   aria-label={currentStep === tutorialSteps.length - 1 ? "Finish tutorial" : "Next step"}
                 >
                   <ChevronRight className="w-6 h-6" />
